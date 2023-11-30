b43f80ffcce373eb5eba962441cfd9b372dd79f8
cyrils <nigthguarder@gmail.com>
2023-11-30 22:13:10 +0100

    add Database Example

--- src/controllers/api.js
+++ src/controllers/api.js
@@ -2,8 +2,19 @@
 import express from 'express'
 const router = express.Router()
 
-router.get('/greeter/:name', function(req, res) {
-    res.json({message: `Hello ${req.params.name}`})
+// Users Example database
+const usersDb = [
+    { id: 1, name: 'Rostislav Javdavan', email: 'jadavan@example.com' },
+    { id: 2, name: 'Cyril', email: 'cyril@example.com' },
+    // Add more users as needed
+];
+
+router.get('/users/:id', function(req, res) {
+    const selected = parseInt(req.params.id)
+    const selectedUser = usersDb.find(user => user.id === selected);
+    
+    // Return the user or an error response
+    res.json(selectedUser ? { SelectedUser: selectedUser } : { error: 'User not found' });
 });
 
 export default router
