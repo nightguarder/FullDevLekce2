16458b93cadd6acb8472404c092be617fef68d79
cyrils <nigthguarder@gmail.com>
2023-11-30 21:20:08 +0100

    personalised greeter GET request

--- src/controllers/view.js
+++ src/controllers/view.js
@@ -1,6 +1,10 @@
 import express from 'express'
 const router = express.Router()
 
+
+// Handle GET requests to the path /greeter/:name
+// Kdokoliv zavola tento get request dostane greeter.ejs odpoved s name
+// http://localhost/greeter/Cyril
 router.get('/greeter/:name', function(req, res) {
     res.render('greeter', { name: req?.params?.name });
 });
